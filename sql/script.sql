35db4df9c3ce80cf77470747c1bda5bce6443d80
Nehal Shah <nehals@adamsbook.com>
2013-10-25 14:16:23 -0400

    Updated stored procedure to update/insert staged data.

--- sql/script.sql
+++ sql/script.sql
@@ -272,251 +272,200 @@ BEGIN
 
 	#Update existing records, then insert new ones
 
-		UPDATE 		`onix`.`productmasterfile` pmf,
-				(
-					SELECT	
-							`ONIXProductRef`,
-							`PPN`,
-							`ISBN10`,
-							`ISBN13`,
-							`EAN13`,
-							`UPC`,
-							`ISMN`,
-							`DOI`,
-							`LCCN`,
-							`GTIN14`,
-							`Title`,
-							`SubTitle`,
-							`SeriesTitle`,
-							`TTitle`,
-							`NPTTitle`,
-							`NPSubTitle`,
-							`NPSeriesTitle`,
-							`Author`,
-							`Illustrator`,
-							`Translator`,
-							`Imprint`,
-							`Supplier`,
-							`Copyright`,
-							`Edition`,
-							`CaseQty`,
-							`Height`,
-							`Width`,
-							`Depth`,
-							`DimensionsUM`,
-							`Weight`,
-							`WeightUM`,
-							`Format`,
-							`FormDetail`,
-							`FormatDescription`,
-							`Availability`,
-							`Expected`,
-							`Price`,
-							`NetPrice`,
-							`Denomination`,
-							`Opened`,
-							`Updated`,
-							`Descriptions`,
-							`StockedBy`,
-							`deducedPrice`,
-							`supplierOfOrigin`,
-							`bisac`
-					FROM	`onix`.`productmasterfile_stage` pmfs_derived
-					WHERE	pmfs_derived.id = id
-				) pmfs
+		SET SQL_SAFE_UPDATES=0;
+
+		UPDATE 		onix.productmasterfile pmf,
+					onix.productmasterfile_stage pmfs
 
 		SET
-					pmf.`ONIXProductRef` = pmfs.ONIXProductRef,
-					pmf.`PPN` = pmfs.PPN,
-					pmf.`ISBN10` = pmfs.ISBN10,
-					pmf.`RevISBN10` = REVERSE(pmfs.ISBN10),
-					pmf.`ISBN13` = pmfs.ISBN13,
-					pmf.`RevISBN13` = REVERSE(pmfs.ISBN13),
-					pmf.`EAN13` = pmfs.EAN13,
-					pmf.`UPC` = pmfs.UPC,
-					pmf.`ISMN` = pmfs.ISMN,
-					pmf.`DOI` = pmfs.DOI,
-					pmf.`LCCN` = pmfs.LCCN,
-					pmf.`GTIN14` = pmfs.GTIN14,
-					pmf.`Title` = pmfs.Title,
-					pmf.`SubTitle` = pmfs.SubTitle,
-					pmf.`SeriesTitle` = pmfs.SeriesTitle,
-					pmf.`TTitle` = pmfs.TTitle,
-					pmf.`NPTTitle` = pmfs.NPTTitle,
-					pmf.`NPSubTitle` = pmfs.NPSubTitle,
-					pmf.`NPSeriesTitle` = pmfs.NPSeriesTitle,
-					pmf.`Author` = pmfs.Author,
-					pmf.`Illustrator` = pmfs.Illustrator,
-					pmf.`Translator` = pmfs.Translator,
-					pmf.`Imprint` = pmfs.Imprint,
-					pmf.`Supplier` = pmfs.Supplier,
-					pmf.`Copyright` = pmfs.Copyright,
-					pmf.`Edition` = pmfs.Edition,
-					pmf.`CaseQty` = pmfs.CaseQty,
-					pmf.`Height` = pmfs.Height,
-					pmf.`Width` = pmfs.Width,
-					pmf.`Depth` = pmfs.Depth,
-					pmf.`DimensionsUM` = pmfs.DimensionsUM,
-					pmf.`Weight` = pmfs.Weight,
-					pmf.`WeightUM` = pmfs.WeightUM,
-					pmf.`Format` = 
-						CASE WHEN EXISTS (SELECT id FROM onix.productformat WHERE id = `Format`)
-						THEN `Format` 
+					pmf.ONIXProductRef = pmfs.ONIXProductRef,
+					pmf.PPN = pmfs.PPN,
+					pmf.ISBN10 = pmfs.ISBN10,
+					pmf.RevISBN10 = REVERSE(pmfs.ISBN10),
+					pmf.ISBN13 = pmfs.ISBN13,
+					pmf.RevISBN13 = REVERSE(pmfs.ISBN13),
+					pmf.EAN13 = pmfs.EAN13,
+					pmf.UPC = pmfs.UPC,
+					pmf.ISMN = pmfs.ISMN,
+					pmf.DOI = pmfs.DOI,
+					pmf.LCCN = pmfs.LCCN,
+					pmf.GTIN14 = pmfs.GTIN14,
+					pmf.Title = pmfs.Title,
+					pmf.SubTitle = pmfs.SubTitle,
+					pmf.SeriesTitle = pmfs.SeriesTitle,
+					pmf.TTitle = pmfs.TTitle,
+					pmf.NPTTitle = pmfs.NPTTitle,
+					pmf.NPSubTitle = pmfs.NPSubTitle,
+					pmf.NPSeriesTitle = pmfs.NPSeriesTitle,
+					pmf.Author = pmfs.Author,
+					pmf.Illustrator = pmfs.Illustrator,
+					pmf.Translator = pmfs.Translator,
+					pmf.Imprint = pmfs.Imprint,
+					pmf.Supplier = pmfs.Supplier,
+					pmf.Copyright = pmfs.Copyright,
+					pmf.Edition = pmfs.Edition,
+					pmf.CaseQty = pmfs.CaseQty,
+					pmf.Height = pmfs.Height,
+					pmf.Width = pmfs.Width,
+					pmf.Depth = pmfs.Depth,
+					pmf.DimensionsUM = pmfs.DimensionsUM,
+					pmf.Weight = pmfs.Weight,
+					pmf.WeightUM = pmfs.WeightUM,
+					pmf.Format = 
+						CASE WHEN EXISTS (SELECT id FROM onix.productformat WHERE id = pmfs.Format)
+						THEN pmfs.Format
 						ELSE '--'
 						END,
-					pmf.`FormDetail` = pmfs.FormDetail,
-					pmf.`FormatDescription` = pmfs.FormatDescription,
-					pmf.`Availability` = 
-						CASE WHEN EXISTS (SELECT id FROM availabilitystatuscode WHERE id = `Availability`)
-						THEN `Availability` 
+					pmf.FormDetail = pmfs.FormDetail,
+					pmf.FormatDescription = pmfs.FormatDescription,
+					pmf.Availability = 
+						CASE WHEN EXISTS (SELECT id FROM AvailabilityStatusCode WHERE id = pmfs.Availability)
+						THEN pmfs.Availability
 						ELSE '--'
 						END,
-					pmf.`Expected` = pmfs.Expected,
-					pmf.`Price` = pmfs.Price,
-					pmf.`NetPrice` = pmfs.NetPrice,
-					pmf.`Denomination` = pmfs.Denomination,
-					pmf.`Opened` = pmfs.Opened,
-					pmf.`Updated` = pmfs.Updated,
-					pmf.`Descriptions` = pmfs.Descriptions,
-					pmf.`StockedBy` = 0,
+					pmf.Expected = pmfs.Expected,
+					pmf.Price = pmfs.Price,
+					pmf.NetPrice = pmfs.NetPrice,
+					pmf.Denomination = pmfs.Denomination,
+					pmf.Updated = pmfs.Updated,
+					pmf.Descriptions = pmfs.Descriptions,
+					pmf.StockedBy = 0,
 						#CASE WHEN EXISTS (
 						#	SELECT	ISBN
 						#	FROM	adamsbook..StockItems
-						#	WHERE	ISBN = `ISBN10`	
-						#	OR		ISBN = `ISBN13`
+						#	WHERE	ISBN = pmfs.ISBN10	
+						#	OR		ISBN = pmfs.ISBN13
 						#)
 						#THEN 1 
 						#ELSE 0
 						#END,
-					pmf.`deducedPrice` = pmfs.deducedPrice,
-					pmf.`supplierOfOrigin` = pmfs.supplierOfOrigin,
-					pmf.`bisac` = pmfs.bisac
+					pmf.deducedPrice = pmfs.deducedPrice,
+					pmf.supplierOfOrigin = pmfs.supplierOfOrigin,
+					pmf.bisac = pmfs.bisac
 		WHERE 
-					`id` = (
-						SELECT 	p.id 
-						FROM 	onix.productmasterfile p
-						WHERE	p.ISBN13 = pmfs.ISBN13
-						OR		p.ISBN10 = pmfs.ISBN10
-						OR 		p.EAN13 = pmfs.EAN13
-						OR 		p.ISMN = pmfs.ISMN
-						OR 		p.PPN = pmfs.PPN
-					);
+					pmf.ISBN13 = pmfs.ISBN13
+			OR		pmf.ISBN10 = pmfs.ISBN10
+			OR 		pmf.EAN13 = pmfs.EAN13
+			OR 		pmf.ISMN = pmfs.ISMN
+			OR 		pmf.PPN = pmfs.PPN;
 
-		INSERT INTO `onix`.`productmasterfile` (
+	# Insert the rest
 
-					`ONIXProductRef`,
-					`PPN`,
-					`ISBN10`,
-					`RevISBN10`,
-					`ISBN13`,
-					`RevISBN13`,
-					`EAN13`,
-					`UPC`,
-					`ISMN`,
-					`DOI`,
-					`LCCN`,
-					`GTIN14`,
-					`Title`,
-					`SubTitle`,
-					`SeriesTitle`,
-					`TTitle`,
-					`NPTTitle`,
-					`NPSubTitle`,
-					`NPSeriesTitle`,
-					`Author`,
-					`Illustrator`,
-					`Translator`,
-					`Imprint`,
-					`Supplier`,
-					`Copyright`,
-					`Edition`,
-					`CaseQty`,
-					`Height`,
-					`Width`,
-					`Depth`,
-					`DimensionsUM`,
-					`Weight`,
-					`WeightUM`,
-					`Format`,
-					`FormDetail`,
-					`FormatDescription`,
-					`Availability`,
-					`Expected`,
-					`Price`,
-					`NetPrice`,
-					`Denomination`,
-					`Opened`,
-					`Updated`,
-					`Descriptions`,
-					`StockedBy`,
-					`deducedPrice`,
-					`supplierOfOrigin`,
-					`bisac`)
+		INSERT INTO onix.productmasterfile (
+
+					ONIXProductRef,
+					PPN,
+					ISBN10,
+					RevISBN10,
+					ISBN13,
+					RevISBN13,
+					EAN13,
+					UPC,
+					ISMN,
+					DOI,
+					LCCN,
+					GTIN14,
+					Title,
+					SubTitle,
+					SeriesTitle,
+					TTitle,
+					NPTTitle,
+					NPSubTitle,
+					NPSeriesTitle,
+					Author,
+					Illustrator,
+					Translator,
+					Imprint,
+					Supplier,
+					Copyright,
+					Edition,
+					CaseQty,
+					Height,
+					Width,
+					Depth,
+					DimensionsUM,
+					Weight,
+					WeightUM,
+					Format,
+					FormDetail,
+					FormatDescription,
+					Availability,
+					Expected,
+					Price,
+					NetPrice,
+					Denomination,
+					Opened,
+					Updated,
+					Descriptions,
+					StockedBy,
+					deducedPrice,
+					supplierOfOrigin,
+					bisac)
 		SELECT		
-					`ONIXProductRef`,
-					`PPN`,
-					`ISBN10`,
-					REVERSE(`ISBN10`),
-					`ISBN13`,
-					REVERSE(`ISBN13`),
-					`EAN13`,
-					`UPC`,
-					`ISMN`,
-					`DOI`,
-					`LCCN`,
-					`GTIN14`,
-					`Title`,
-					`SubTitle`,
-					`SeriesTitle`,
-					`TTitle`,
-					`NPTTitle`,
-					`NPSubTitle`,
-					`NPSeriesTitle`,
-					`Author`,
-					`Illustrator`,
-					`Translator`,
-					`Imprint`,
-					`Supplier`,
-					`Copyright`,
-					`Edition`,
-					`CaseQty`,
-					`Height`,
-					`Width`,
-					`Depth`,
-					`DimensionsUM`,
-					`Weight`,
-					`WeightUM`,
-					CASE WHEN EXISTS (SELECT id FROM onix.productformat WHERE id = `Format`)
-					THEN `Format` 
+					ONIXProductRef,
+					PPN,
+					ISBN10,
+					REVERSE(ISBN10),
+					ISBN13,
+					REVERSE(ISBN13),
+					EAN13,
+					UPC,
+					ISMN,
+					DOI,
+					LCCN,
+					GTIN14,
+					Title,
+					SubTitle,
+					SeriesTitle,
+					TTitle,
+					NPTTitle,
+					NPSubTitle,
+					NPSeriesTitle,
+					Author,
+					Illustrator,
+					Translator,
+					Imprint,
+					Supplier,
+					Copyright,
+					Edition,
+					CaseQty,
+					Height,
+					Width,
+					Depth,
+					DimensionsUM,
+					Weight,
+					WeightUM,
+					CASE WHEN EXISTS (SELECT id FROM onix.ProductFormat WHERE id = Format)
+					THEN Format
 					ELSE '--'
 					END,
-					`FormDetail`,
-					`FormatDescription`,
-					CASE WHEN EXISTS (SELECT id FROM onix.availabilitystatuscode WHERE id = `Availability`)
-					THEN `Availability` 
+					FormDetail,
+					FormatDescription,
+					CASE WHEN EXISTS (SELECT id FROM onix.AvailabilityStatusCode WHERE id = Availability)
+					THEN Availability 
 					ELSE '--'
 					END,
-					`Expected`,
-					`Price`,
-					`NetPrice`,
-					`Denomination`,
-					`Opened`,
-					`Updated`,
-					`Descriptions`,
+					Expected,
+					Price,
+					NetPrice,
+					Denomination,
+					Opened,
+					Updated,
+					Descriptions,
 					0, #CASE WHEN EXISTS (
 					#	SELECT	ISBN
 					#	FROM	adamsbook..StockItems
-					#	WHERE	ISBN = `ISBN10`	
-					#	OR		ISBN = `ISBN13`
+					#	WHERE	ISBN = ISBN10	
+					#	OR		ISBN = ISBN13
 					#)
 					#THEN 1 
 					#ELSE 0
 					#END,
-					`deducedPrice`,
-					`supplierOfOrigin`,
-					`bisac`
+					deducedPrice,
+					supplierOfOrigin,
+					bisac
 		FROM 		
-					`onix`.`productmasterfile_stage`
+					onix.productmasterfile_stage
 		WHERE
 					ISBN13 NOT IN (SELECT ISBN13 FROM onix.productmasterfile)
 		and			ISBN10 NOT IN (SELECT ISBN10 FROM onix.productmasterfile)
@@ -524,4 +473,7 @@ BEGIN
 		and			ISMN NOT IN (SELECT ISMN FROM onix.productmasterfile)
 		and			PPN NOT IN (SELECT PPN FROM onix.productmasterfile);
 
-END
+		SET SQL_SAFE_UPDATES=1;
+
+END$$
+DELIMITER ;
